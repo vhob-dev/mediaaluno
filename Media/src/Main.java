fe7f2410f17e91747175444bd2a85f23c6115c62
vhob-dev <dev.vhobtrabalhos@gmail.com>
2024-07-01 14:58:07 -0300

    first commit

---
+++ Media/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+         int n1 = 6;
+         int n2 = 6;
+         int n3 = 4;
+         int n4 = 6;
+         float m = (float) (n1 + n2 + n3 + n4) /4;
+         System.out.println("a media e igual " + m);
+
+
+        }
+    }
